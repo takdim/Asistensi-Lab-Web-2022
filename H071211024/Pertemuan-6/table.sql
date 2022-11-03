ecab2d3102e7ed21283006c5655ed3135e5854dd
YusranHS <114170755+YusranHS@users.noreply.github.com>
2022-11-03 14:44:23 +0800

    Add files via upload

---
+++ H071211024/Pertemuan-6/table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `mahasiswa` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nim` varchar(25) NOT NULL,
+  `nama` varchar(25) NOT NULL,
+  `alamat` varchar(25) NOT NULL,
+  `fakultas` varchar(25) NOT NULL,
+  PRIMARY KEY (`id`)
+)
