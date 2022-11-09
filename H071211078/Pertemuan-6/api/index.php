4435dc17e289b485bad83ebe02a781de66779b47
FjrSdq <92590349+FjrSdq@users.noreply.github.com>
2022-11-09 14:12:06 +0800

    Recovering deleted php tag
    Ternyata tag kosong dibawah ada gunanya, sebagai penutup kurung kurawal di line 46

--- H071211078/Pertemuan-6/api/index.php
+++ H071211078/Pertemuan-6/api/index.php
@@ -63,6 +63,9 @@
                             </button>
                         </td>
                     </tr>
+                    <?php
+                    }
+                    ?>
             </tbody>
         </table>
     </div>
