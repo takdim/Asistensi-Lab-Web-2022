d28739be70a4403a5fb8315af2da5c51c328c74d
FjrSdq <92590349+FjrSdq@users.noreply.github.com>
2022-11-10 21:43:14 +0800

    Tugas Praktikum 7

---
+++ H071211078/Pertemuan 7/api/db_conn.php
@@ -0,0 +1,8 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "prtk_6";
+$port = 3307;
+
+$conn = mysqli_connect($servername, $username, $password, $dbname, $port);
